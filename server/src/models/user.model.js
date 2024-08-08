fd7288df3bc42e0066b845f42d8cb66a778cc17e
Yohannes Walle <etawe1627@gmail.com>
2024-08-07 19:17:54 -1200

    user model created

---
+++ server/src/models/user.model.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+
+const userModel = new Schema({
+
+    fullName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    gender: {
+        type: String,
+        required: true,
+        enum: ['male', 'female'],
+        trim: true
+    },
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        minlength: 3,
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 8,
+    },
+
+}, { timestamps: true });
+
+module.exports = model('User', userModel);
