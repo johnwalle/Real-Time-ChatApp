ab78c4822affff0d57ca5e77d0617b9a95419eb1
Yohannes Walle <etawe1627@gmail.com>
2024-08-11 13:04:30 -1200

    jwtExpires modified

--- server/src/utils/generateToken.js
+++ server/src/utils/generateToken.js
@@ -2,7 +2,7 @@
 const jwt = require('jsonwebtoken');
 
 const generateToken = (userId) => {
-    return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '30d' });
 };
 
 module.exports = generateToken;
